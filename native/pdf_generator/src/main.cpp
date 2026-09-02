44c3e118a3f95ce988d550b1698b77449dfbd7d8
Shayan <shayan.jafarzadeh@gmail.com>
2026-09-02 14:49:55 +0200

    NAT-12 PDF generator

--- native/pdf_generator/src/main.cpp
+++ native/pdf_generator/src/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Nordiska PDF generator ready\n";
+    constexpr int expectedArgumentCount = 3; // program name + input JSON + output PDF, to avoid magic numbers!
+
+    if (argc != expectedArgumentCount) // runs when argument count is incorrect
+    { 
+        std::cerr << "Usage: " << argv[0] // for .NET to seperate error messages from normal output (CERR)
+        << " <input-json> <output-pdf>\n"; // print expected arguments
+        return 1;   
+    
+    }
+
+
+    const std::string inputpath = argv[1]; //  converts C-Style string to C++ string
+    const std::string outputpath = argv[2];
+
+    std::cout << "Input: " << inputpath << "\n";
+    std::cout << "Output: " << outputpath << "\n";
+
     return 0;
+
 }
