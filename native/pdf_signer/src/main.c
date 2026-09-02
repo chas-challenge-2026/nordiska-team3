55e09e2e1c8b3a78c7b764c3806014140d922801
theredguylmao <gahramanov444@gmail.com>
2026-09-02 16:05:43 +0200

    feature(NAT-13): added signer CLI argument handling

--- native/pdf_signer/src/main.c
+++ native/pdf_signer/src/main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+int main(int argc, char* argv[]) //argv used later to call real data, I suppose.
 {
-    printf("Nordiska PDF signer ready\n");
-    return 0;
+    const int expectedArgumentCount = 5;
+
+    if (argc != expectedArgumentCount)
+    {
+        fprintf(stderr,
+                "Usage:\n"
+                "  %s sign <input-pdf> <private-key-pem> "
+                "<output-signature>\n"
+                "  %s verify <input-pdf> <public-key-pem> "
+                "<signature-file>\n",
+                argv[0],
+                argv[0]);
+        
+        return 1;
+    }
+
+    const char* operation = argv[1];
+
+    if (strcmp(operation, "sign") == 0)
+    {
+        printf("Operation: SIGN\n");
+        printf("Input PDF: %s\n", argv[2]);
+        printf("Private Key: %s\n", argv[3]);
+        printf("Output Signature: %s\n", argv[4]);
+
+        return 0;
+    }
+
+    if (strcmp(operation, "verify") == 0)
+    {
+        printf("Operation: VERIFY\n");
+        printf("Input PDF: %s\n", argv[2]);
+        printf("Public Key: %s\n", argv[3]);
+        printf("Signature: %s\n", argv[4]);
+
+        return 0;
+    }
+
+    fprintf(stderr, "Error: Unknown operation - '%s'\n", operation);
+
+    return 1;
 }
