7ad5f6035dca3597339191aa2bcc3f43c5d3b307
agnesvarnlund <208608641+agnes-varnlund@users.noreply.github.com>
2026-09-03 11:39:26 +0200

    Lagt till React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/LoginPage/LoginPage'
 
 function App() {
-  return <LoginPage />
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  )
 }
 
 export default App
