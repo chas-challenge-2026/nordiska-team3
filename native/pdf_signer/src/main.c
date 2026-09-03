e6805e3b5f04249aba8ba127c67304cb559cfb4c
theredguylmao <gahramanov444@gmail.com>
2026-09-03 09:37:58 +0200

    added a small tutorial in pdf_signer

--- native/pdf_signer/src/main.c
+++ native/pdf_signer/src/main.c
@@ -1,3 +1,53 @@
+// main.c
+
+/*  
+    Contains a simple program that takes the input of 'user' 
+    and and gives out the assigned arguments. 
+    There are only 5 arguments used. 
+    Currently serves as a small draft of a complete function.
+
+    To test, first you need to build the project: 
+    (make sure you have all required libraries installed)
+
+    '''bash
+    cd "D:/'yourpath'/nordiska-team3"
+
+    cmake -S native -B native/build
+    cmake --build native/build
+    '''
+    You can change the name of the folder if you just
+    want to test it without making mess in actual 'build' folder.
+
+
+    '''bash
+    ./native/build 'prompt' report.pdf private-key.pem report.sig
+    '''
+
+    In 'prompt' write either 'sign' or 'verify'.
+
+    No input will result in a tutorial:
+        Usage:
+        ./native/build/pdf_signer/pdf_signer sign
+            <input-pdf> <private-key-pem> <output-signature>
+        ./native/build/pdf_signer/pdf_signer verify 
+            <input-pdf> <public-key-pem> <signature-file>
+
+    'sign' and 'verify' input results:
+
+        Operation: SIGN
+        Input PDF: report.pdf
+        Private Key: private_key.pem
+        Output Signature: report.sig
+
+        Operation: VERIFY
+        Input PDF: report.pdf
+        Public Key: private_key.pem
+        Signature: report.sig
+    
+    Invalid input result:
+        Error: Unknown operation - '(invalid input)'
+*/ 
+
 #include <stdio.h>
 #include <string.h>
 
